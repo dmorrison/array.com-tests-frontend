8ee6bc8e6f7559f8282c0d96887ca87db8ec800b
Derek Morrison <me@derek-morrison.com>
2022-09-24 04:21:20 -0500

    Wire up show/hide history

--- components/credit-lock-center/index.js
+++ components/credit-lock-center/index.js
@@ -14,6 +14,47 @@ class CreditLockCenter extends BaseComponent {
     };
     super(templatePath, themesInfo);
   }
+
+  async connectedCallback() {
+    await super.connectedCallback();
+
+    const historyTitleElem = this.shadowRoot.querySelector(".history-title");
+    historyTitleElem.addEventListener("click", this.historyTitleClickHandler);
+  }
+ 
+  disconnectedCallback() {
+    const historyTitleElem = this.shadowRoot.querySelector(".history-title");
+    historyTitleElem.removeEventListener("click", this.historyTitleClickHandler);
+  }
+
+  historyTitleClickHandler(event) {
+    const historyTitleElem = event.target;
+    const historyListPanelElem = historyTitleElem.closest(".history-list-panel");
+
+    // Switch text to "Show" or "Hide".
+    const title = historyTitleElem.innerText;
+    if (title.indexOf("Show") === 0) {
+      historyTitleElem.innerText = title.replace("Show", "Hide");
+    } else {
+      historyTitleElem.innerText = title.replace("Hide", "Show");
+    }
+
+    // Toggle visibility for the history list.
+    const historyListWrapperElem = historyListPanelElem.querySelector(".history-list-wrapper");
+    if (historyListWrapperElem.style.display === "none") {
+      historyListWrapperElem.style.display = "block";
+    } else {
+      historyListWrapperElem.style.display = "none";
+    }
+
+    // Toggle visibility for the "Show All" link/button.
+    const showAllElem = historyListPanelElem.querySelector(".show-all");
+    if (showAllElem.style.display === "none") {
+      showAllElem.style.display = "block";
+    } else {
+      showAllElem.style.display = "none";
+    }
+  }
 }
 
 customElements.define("credit-lock-center", CreditLockCenter);
